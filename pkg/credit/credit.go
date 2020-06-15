748fad93904bc23c1e50207eed9da67561b7c47d
ArtDark <balusoff@gmail.com>
2020-06-15 13:10:40 +0300

    fix code

--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -3,9 +3,9 @@ package credit
 import "math"
 
 func Calculate(credit, period, percent int) (monthly, over, total int) {
-	monthlyPercent := float64(percent) / 12 / 100 // тут я округлил чтобы получилось 0,017
+	monthlyPercent := float64(percent) / 12 / 100
 
-	exp := math.Pow(1+monthlyPercent, float64(period)) // 1,834654562
+	exp := math.Pow(1+monthlyPercent, float64(period))
 	coefficient := (monthlyPercent * exp) / (exp - 1)
 
 	monthly = int(coefficient * float64(credit))
